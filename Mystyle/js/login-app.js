9bf4c5d45e2ba51c4c6abfbc33f38bd83d42f6c4
NaToTheFla-nafla <62860757+NaToTheFla-nafla@users.noreply.github.com>
2022-02-25 17:02:16 +0800

    提交文件

---
+++ Mystyle/js/login-app.js
@@ -0,0 +1,42 @@
+const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
+
+// 设置label悬浮样式
+inputs.forEach((inp) => {
+  inp.addEventListener("focus", () => {
+    inp.classList.add("active");
+  });
+  inp.addEventListener("blur", () => {
+    if (inp.value != "") return;
+    inp.classList.remove("active");
+  });
+});
+
+// 设置登录注册表单左右转换
+toggle_btn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    main.classList.toggle("sign-up-mode");
+  });
+});
+
+// 设置圆点的变化以及图片文字的转化  使文本转化时上下转换
+function moveSlider() {
+  let index = this.dataset.value;
+
+  let currentImage = document.querySelector(`.img-${index}`);
+  images.forEach((img) => img.classList.remove("show"));
+  currentImage.classList.add("show");
+
+  const textSlider = document.querySelector(".text-group");
+  textSlider.style.transform = `translateY(${-(index - 1) * 2.2}rem)`;
+
+  bullets.forEach((bull) => bull.classList.remove("active"));
+  this.classList.add("active");
+}
+
+bullets.forEach((bullet) => {
+  bullet.addEventListener("click", moveSlider);
+});
---
+++ Mystyle/js/wscript.js
@@ -0,0 +1,162 @@
+// 让搜索框动态显示
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+  navbar.classList.remove('active');
+}
+
+// 让主体指引动态显示
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+  navbar.classList.toggle('active');
+  searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+  searchForm.classList.remove('active');
+  navbar.classList.remove('active');
+}
+
+// 点击索引标题后到达指示位置
+let filterBtn = document.querySelectorAll('.filter-buttons .buttons');
+let filterItem = document.querySelectorAll('.clothes .box-container .box');
+
+filterBtn.forEach(button =>{
+
+  button.onclick = () =>{
+    filterBtn.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+
+    let dataFilter = button.getAttribute('data-filter');
+
+    filterItem.forEach(item =>{
+
+      item.classList.remove('active');
+      item.classList.add('hide');
+
+      if(item.getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+        item.classList.remove('hide');
+        item.classList.add('active');
+      }
+
+    });
+
+  };
+
+});
+
+// 使图片可以滑动展示
+var swiper = new Swiper(".home-slider", {
+  centeredSlides: true,
+  loop:true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".shoes-slider", {
+  centeredSlides: true,
+  loop:true,
+  spaceBetween:20,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".ACC-slide", {
+  loop:true,
+  spaceBetween:20,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+  },
+});
+
+var swiper = new Swiper(".OOTD-slider", {
+  centeredSlides: true,
+  loop:true,
+  spaceBetween:20,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// clothes添加图片部分
+function ProcessFile(e) {
+  var file = document.getElementById('file').files[0];
+  if (file) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+          var txt = event.target.result;
+
+          var img = document.createElement("img");
+          img.src = txt;//将图片base64字符串赋值给img的src
+         // console.log(txt);
+          document.getElementById("result").appendChild(img);
+      };
+  }
+  reader.readAsDataURL(file);
+}
+
+function contentLoaded() {
+  document.getElementById('file').addEventListener('change',
+      ProcessFile, false);
+}
+
+window.addEventListener("DOMContentLoaded", contentLoaded, false);
+
+    
